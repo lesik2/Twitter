dc0d75f9b0c05fbd2e0e0edfff88952db93d38fb
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2024-01-09 02:12:13 +0300

    fix: update tests

--- src/tests/components/DateChoose.test.tsx
+++ src/tests/components/DateChoose.test.tsx
@@ -1,10 +1,10 @@
-import { getArrayOfNumbers, getDays } from '@components/DateChoose/helpers/index';
-import { DateChoose } from '@components/DateChoose/index';
+import { getArrayOfNumbers, getDays } from '@components/DateDropDown/helpers/index';
+import { DateDropDown } from '@components/DateDropDown/index';
 import { IDateChoose } from '@customTypes/index';
 
 import { render, screen } from '../test.utils';
 
-describe('DateChoose', () => {
+describe('DateDropDown', () => {
   test('Should return array of numbers between start and end with start and end', () => {
     expect(getArrayOfNumbers(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     expect(getArrayOfNumbers(2015, 2020)).toEqual([2015, 2016, 2017, 2018, 2019, 2020]);
@@ -20,13 +20,13 @@ describe('DateChoose', () => {
     const februaryDays = new Date(currentYear, 2, 0).getDate();
     expect(getDays({ month: 1 }).length).toBe(februaryDays);
   });
-  test('Should render DateChoose component', () => {
+  test('Should render DateDropDown component', () => {
     const props: IDateChoose = {
       date: {},
       setDate: jest.fn(),
     };
 
-    render(<DateChoose {...props} />);
+    render(<DateDropDown {...props} />);
     expect(screen.getByTestId('date-choose')).toBeInTheDocument();
   });
   test('Should render 3 DropDown', () => {
@@ -35,7 +35,7 @@ describe('DateChoose', () => {
       setDate: jest.fn(),
     };
 
-    render(<DateChoose {...props} />);
+    render(<DateDropDown {...props} />);
     expect(screen.getByTestId('date-choose').children.length).toBe(3);
   });
 });
